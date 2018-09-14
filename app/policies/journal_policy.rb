1b305db5237eb372f310e9e56e232b666587f3af
Maria Castro <mariacastro@aquaporservicos.pt>
2018-09-14 23:59:21 +0100

    CRUD in controller done. Authorizations are for admins as for now. come back to that later (trello)

--- app/policies/journal_policy.rb
+++ app/policies/journal_policy.rb
@@ -4,8 +4,28 @@ class JournalPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def index?
+    user.admin?
+  end
+
   def show?
     #record.user = the user that belongs to the record
     record.user == user || user.admin?
+  end
+
+  def create?
+    #record.user = the user that belongs to the record
+    user.admin?
+  end
+
+  def update?
+    #record.user = the user that belongs to the record
+    user.admin?
+  end
+
+  def destroy?
+    #record.user = the user that belongs to the record
+    user.admin?
   end
 end
