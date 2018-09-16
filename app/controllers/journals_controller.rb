d11003a537c16b38b56d1a60a2a660f0a97aa715
Maria Castro <mariacastro@aquaporservicos.pt>
2018-09-16 13:40:03 +0100

    policies for crud done

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -3,6 +3,8 @@ class JournalsController < ApplicationController
   before_action :set_journal, only: [:show, :edit, :update, :destroy]
 
   def index
+    @journals = Journal.all
+    authorize @journals
   end
 
   def show
@@ -10,23 +12,48 @@ class JournalsController < ApplicationController
   end
 
   def new
+    @journal = Journal.new
+    authorize @journal
   end
 
   def create
+    @journal = Journal.new(journal_params)
+    authorize @journal
+    if @journal.save
+      redirect_to @journal, notice: :created
+    else
+      render :new
+    end
   end
 
   def edit
+    authorize @journal
   end
 
   def update
+    authorize @journal
+    if @journal.update(journal_params)
+      redirect_to @journal, notice: :updated
+    else
+      render :edit
+    end
   end
 
   def destroy
+    authorize @journal
+    flash[:notice] = :destroyed
+    @journal.destroy
+    redirect_to @journal
   end
 
   private
 
   def set_journal
     @journal = Journal.find(params[:id])
+  end
+
+  def journal_params
+    # TODO: increase security
+    params.require(:journal).permit!
   end
 end
--- app/policies/journal_policy.rb
+++ app/policies/journal_policy.rb
@@ -4,8 +4,25 @@ class JournalPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def index?
+    user.admin?
+  end
+
   def show?
     #record.user = the user that belongs to the record
     record.user == user || user.admin?
+  end
+
+  def create?
+    user.admin?
+  end
+
+  def update?
+    user.admin?
+  end
+
+  def destroy?
+    user.admin?
   end
 end
